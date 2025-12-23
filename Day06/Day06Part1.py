ce7b2257dd8598cef6126dc0785833bad9e583a9
Andrew Slejzer <223679615+amslejzer@users.noreply.github.com>
2025-12-23 16:22:31 -0500

    Solve day 6, part 1

---
+++ Day06/Day06Part1.py
@@ -0,0 +1,20 @@
+lines = []
+grand_total = 0
+
+with open("Day06/Input.txt", "r") as file:
+    for line in file:
+        lines.append(line.split())
+
+for items in zip(*lines):
+    column_result = 0
+    operation = items[-1]
+    if operation == '+':
+        for iterator in range(len(items) - 1):
+            column_result += int(items[iterator])
+    elif operation == '*':
+        column_result = int(items[0])
+        for iterator in range(1, len(items) - 1):
+            column_result *= int(items[iterator])
+    grand_total += column_result
+
+print(grand_total)
